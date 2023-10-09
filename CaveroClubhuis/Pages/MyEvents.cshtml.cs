7b9f32f03270f9e2e4a229367f57efcff55d6de7
Megan <meganpieters3@gmail.com>
2023-10-09 20:02:29 +0200

    main pages toegevoegd

---
+++ CaveroClubhuis/Pages/MyEvents.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CaveroClubhuis.Pages
+{
+    public class MyEventsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
