aa4eb402165e837367eda307daef3906b0925025
maruf128 <1052505@hr.nl>
2023-11-13 13:07:58 +0100

    raw data in kalender :D

--- CaveroClubhuis/Pages/Kalender.cshtml.cs
+++ CaveroClubhuis/Pages/Kalender.cshtml.cs
@@ -1,12 +1,30 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using CaveroClubhuis.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
 
 namespace CaveroClubhuis.Pages
 {
     public class KalenderModel : PageModel
     {
+        private readonly CaveroClubhuisContext _context;
+
+        public KalenderModel(CaveroClubhuisContext context)
+        {
+            _context = context;
+        }
+
+        public List<Events> EventsList { get; set; }
+
         public void OnGet()
         {
+            EventsList = FetchEvents();
+        }
+
+        public List<Events> FetchEvents()
+        {
+            // Fetch all events from the database
+            return _context.Events.ToList();
         }
     }
 }
