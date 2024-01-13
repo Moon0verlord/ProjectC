1751adc77324b33b69435ef2469d10378392a14d
Moon0verlord <114489960+Moon0verlord@users.noreply.github.com>
2024-01-13 12:03:35 +0100

    Add top margin to Log-out class in Layout.css and remove comment in CaveroUser.cs

--- CaveroClubhuis/Areas/Identity/Data/CaveroUser.cs
+++ CaveroClubhuis/Areas/Identity/Data/CaveroUser.cs
@@ -7,7 +7,6 @@ using Microsoft.AspNetCore.Identity;
 
 namespace CaveroClubhuis.Areas.Identity.Data;
 
-// Add profile data for application users by adding properties to the CaveroUser class
 public class CaveroUser : IdentityUser
 {
     [PersonalData]
